d19e6b02e4083276923f6e8835ff913751b809d0
N129BZ <N129BZ@outlook.com>
2021-09-18 14:24:57 -0500

    modified settings logic

--- index.js
+++ index.js
@@ -12,14 +12,32 @@ var db_history;
 var dbpath_tiles;
 var dbpath_history;
 
-readSettingsFile();
+readSettingsFile(true);
 
-function readSettingsFile() {
+function readSettingsFile(opendbs) {
     let rawdata = fs.readFileSync(__dirname + '/settings.json');
     settings = JSON.parse(rawdata);
     dbpath_tiles = __dirname +"/public/data/" + settings.tiledb;
     dbpath_history = __dirname + "/public/data/" + settings.historydb;
-    
+
+    // get last known lng, lat, and hdg
+    let sql = "SELECT * FROM position_history WHERE id IN ( SELECT max( id ) FROM position_history )";
+    db_history.get(sql, (err, row) => {
+        if (err == null) {
+            if (row != undefined) {
+                settings.lastlongitude = row.longitude;
+                settings.lastlatitude = row.latitude;
+                settings.lastheading = row.heading;
+            }
+        }   
+    });
+
+    if (opendbs) {
+        openDatabases();
+    }
+}
+
+function openDatabases() {
     // open map db read only
     db_tiles = new sqlite3.Database(dbpath_tiles, sqlite3.OPEN_READONLY, (err) => {
         if (err) {
@@ -34,20 +52,7 @@ function readSettingsFile() {
             console.log("Failed to load: " + dbpath_history);
         }
     });
-
-    // get last known lng, lat, and hdg
-    let sql = "SELECT * FROM position_history WHERE id IN ( SELECT max( id ) FROM position_history )";
-    db_history.get(sql, (err, row) => {
-        if (err == null) {
-            if (row != undefined) {
-                settings.lastlongitude = row.longitude;
-                settings.lastlatitude = row.latitude;
-                settings.lastheading = row.heading;
-            }
-        }   
-    });
 }
-
 // express web server  
 let app = express();
 try {
@@ -111,6 +116,7 @@ function putPositionHistory(data) {
 }
 
 function getSettings(response) {
+    readSettingsFile(false);
     response.writeHead(200);
     response.write(JSON.stringify(settings));
     response.end();
